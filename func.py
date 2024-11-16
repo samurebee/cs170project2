8762104c8f9d66650217c12fbfbb59fc7e48e2b5
Chrystal Chavez <cchav083@ucr.edu>
2024-11-15 22:37:58 -0800

    Added comments to forward selection algorithm.

--- func.py
+++ func.py
@@ -2,29 +2,33 @@ import random
 
 def forwardSelection(numOfFeatures):
 
-    features = []
-    highestAccuracy = round(random.random() * 100,1) 
-    featuresWithHighestAccuracy = []
-    accuracyFlag = 0
+    features = [] # list to know what features could be added
+    highestAccuracy = round(random.random() * 100,1) # no features has highest accuracy so far.
+    featuresWithHighestAccuracy = [] # when adding a feature the feature that gives the highest accuracy is added here
+    accuracyFlag = 0 # used to see if the accuracy increases
     print("Using no features and \"random\" evaluation, I get an accuracy of ", highestAccuracy, "%.\n", sep = "")
 
     print("Beginning Search.\n")
 
+    # add feature numbers here based on number of features chosen by user
     for i in range(0, numOfFeatures):
         features.append(i+1)
 
+    # loop until there are no more features to add
     while(len(features) != 0):
-
+        # loop until there are no more features to compare
         for i in range(0,len(features)):
-            accuracy = round(random.random() * 100,1) 
+            accuracy = round(random.random() * 100,1) # assign accuracy to feature or set of features
             print("Using feature(s) {", end ="")
             for j in range(0,len(featuresWithHighestAccuracy)):
                 print(featuresWithHighestAccuracy[j], ",", sep = "", end="")
             print(features[i], "} accuracy is ", accuracy, "%.", sep = "")
+            # checks if there is a set of features with a higher accuracy
             if(accuracy > highestAccuracy):
                 feature = features[i]
-                accuracyFlag = 1
-                highestAccuracy = accuracy
+                accuracyFlag = 1 # accuracy increased
+                highestAccuracy = accuracy # modify highest accuracy
+        # if accuracy does not increase finish the search
         if(accuracyFlag == 0):
             print("\n(Warning, Accuracy has decreased!)")
             print("Finished Search!! The best feature subset is {", end="")
@@ -34,6 +38,7 @@ def forwardSelection(numOfFeatures):
                     print(",", sep = "", end="")
             print("}, which has an accuracy of ", highestAccuracy, "%.\n", sep = "",)
             break
+        # if accuracy increases print set of features with highest accuracy so far and continue search
         else:
             features.remove(feature)
             featuresWithHighestAccuracy.append(feature)
