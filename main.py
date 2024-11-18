43ba0085cb7dc04f845432fa950a11c244e43ffe
sam uribe <samuribe@sams-MacBook-Air.local>
2024-11-17 20:34:04 -0800

    updating ui to handle when user doesnt enter a integer

--- main.py
+++ main.py
@@ -2,9 +2,21 @@ import func
 # User prompts and validate choices
 # Goes to the correct algorithm that the user chooses
 
-numFeatures = input("Please enter total number of features: ")
+while True:
+    numFeatures = input("Please enter total number of features: ")
+    try:
+        val = int(numFeatures)
+        break
+    except ValueError:
+        print("Please enter a integer")
 
-numberAlgorithm = input("\nForward Selection(1)\nBacward Elimination(2)\nSpecial Algorithm(3)\nEnter:")
+while True:
+    numberAlgorithm = input("\nForward Selection(1)\nBacward Elimination(2)\nSpecial Algorithm(3)\nEnter:")
+    try:
+        val = int(numberAlgorithm)
+        break
+    except ValueError:
+        print("Please enter a integer")
 
 if(numberAlgorithm == 1):
     func.forwardSelection(numFeatures)
