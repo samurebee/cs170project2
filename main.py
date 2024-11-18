d23b8cc58e854f705e21ca789125d8b19c761a5e
sam uribe <samuribe@sams-MacBook-Air.local>
2024-11-17 20:29:57 -0800

    skelton ui

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 import func
 # User prompts and validate choices
 # Goes to the correct algorithm that the user chooses
+
+numFeatures = input("Please enter total number of features: ")
+
+numberAlgorithm = input("\nForward Selection(1)\nBacward Elimination(2)\nSpecial Algorithm(3)\nEnter:")
+
+if(numberAlgorithm == 1):
+    func.forwardSelection(numFeatures)
+
+elif(numberAlgorithm == 2):
+    func.backwardSelection(numFeatures)
