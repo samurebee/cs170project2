863e4fcd7b75d20b7e0144f8c0b9872d6acbb7fa
Chrystal Chavez <cchav083@ucr.edu>
2024-11-29 19:41:21 -0800

    Commented out part 1 of main for part 2

--- main.py
+++ main.py
@@ -2,26 +2,26 @@ import func
 # User prompts and validate choices
 # Goes to the correct algorithm that the user chooses
 
-while True:
-    numFeatures = input("Please enter total number of features: ")
-    try:
-        numFeatures = int(numFeatures)
-        break
-    except ValueError:
-        print("Please enter a integer")
+# while True:
+#     numFeatures = input("Please enter total number of features: ")
+#     try:
+#         numFeatures = int(numFeatures)
+#         break
+#     except ValueError:
+#         print("Please enter a integer")
 
-while True:
-    numberAlgorithm = input("\nForward Selection(1)\nBacward Elimination(2)\nSpecial Algorithm(3)\nEnter:")
-    try:
-        numberAlgorithm = int(numberAlgorithm)
-        break
-    except ValueError:
-        print("Please enter a integer")
+# while True:
+#     numberAlgorithm = input("\nForward Selection(1)\nBacward Elimination(2)\nSpecial Algorithm(3)\nEnter:")
+#     try:
+#         numberAlgorithm = int(numberAlgorithm)
+#         break
+#     except ValueError:
+#         print("Please enter a integer")
 
-if(numberAlgorithm == 1):
-    func.forwardSelection(numFeatures)
+# if(numberAlgorithm == 1):
+#     func.forwardSelection(numFeatures)
 
-elif(numberAlgorithm == 2):
-    func.backwardSelection(numFeatures)
+# elif(numberAlgorithm == 2):
+#     func.backwardSelection(numFeatures)
 
 func.NNClassifier([3,5,7])
