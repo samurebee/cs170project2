e5534d40b9be07fdc2ae19feab2d5637dac5b9d3
sam uribe <samuribe@sams-MacBook-Air.local>
2024-12-03 23:18:44 -0800

    part 3

--- func.py
+++ func.py
@@ -1,165 +1,118 @@
-import random
 import pandas as pd
 import time
+import numpy as np
+
 
 def inputFeatures(features, numOfFeatures):
-    for i in range(0, numOfFeatures):
-        features.append(i+1) #adding features 0 to numOfFeatures into features list
+    for i in range(1, numOfFeatures + 1):  # Feature indices start at 1
+        features.append(i)
+
 
 def printFeatures(features):
-    for i in range(0,len(features)):
-        print(features[i], sep = "", end="")
-        if(i != len(features) - 1):
-            print(",", sep = "", end="")
+    for i, feature in enumerate(features):
+        print(feature, end=", " if i < len(features) - 1 else "")
+
 
 def printWarning(features, highestAccuracy):
-    print("\n(Warning, Accuracy has decreased!)")
-    print("Finished Search!! The best feature subset is {", end="")
+    print("\n(Warning: Accuracy decreased!)")
+    print(f"Finished Search! The best feature subset is {{", end="")
     printFeatures(features)
-    print("}, which has an accuracy of ", highestAccuracy, "%.\n", sep = "",)
+    print(f"}}, with an accuracy of {highestAccuracy:.2f}%")
 
 def printBest(features, highestAccuracy):
-    print("\n Feature Set {", end="")
+    print(f"\nFeature set {{", end="")
     printFeatures(features)
-    print("} was best, accuracy is ", highestAccuracy, "%.\n", sep = "",)
+    print(f"}} is best, accuracy is {highestAccuracy:.2f}%")
 
-def forwardSelection(numOfFeatures):
 
-    features = [] # list to know what features could be added
-    highestAccuracy = round(random.random() * 100,1) # no features has highest accuracy so far.
-    featuresWithHighestAccuracy = [] # when adding a feature the feature that gives the highest accuracy is added here
-    accuracyFlag = 0 # used to see if the accuracy increases
-    print("Using no features and \"random\" evaluation, I get an accuracy of ", highestAccuracy, "%.\n", sep = "")
-
-    print("Beginning Search.\n")
-
-    # add feature numbers here based on number of features chosen by user
-    inputFeatures(features, numOfFeatures)
-
-    # loop until there are no more features to add
-    while(len(features) != 0):
-        # loop until there are no more features to compare
-        for i in range(0,len(features)):
-            accuracy = round(random.random() * 100,1) # assign accuracy to feature or set of features
-            print("Using feature(s) {", end ="")
-            for j in range(0,len(featuresWithHighestAccuracy)):
-                print(featuresWithHighestAccuracy[j], ",", sep = "", end="")
-            print(features[i], "} accuracy is ", accuracy, "%.", sep = "")
-            # checks if there is a set of features with a higher accuracy
-            if(accuracy > highestAccuracy):
-                feature = features[i]
-                accuracyFlag = 1 # accuracy increased
-                highestAccuracy = accuracy # modify highest accuracy
-        # if accuracy does not increase finish the search
-        if(accuracyFlag == 0):
-            printWarning(featuresWithHighestAccuracy, highestAccuracy)
-            break
-        # if accuracy increases print set of features with highest accuracy so far and continue search
-        else:
-            features.remove(feature)
-            featuresWithHighestAccuracy.append(feature)
-            printBest(featuresWithHighestAccuracy, highestAccuracy)
-            accuracyFlag = 0
-
-def backwardSelection(numOfFeatures):
-
-    highestAccuracy = round(random.random() * 100,1) 
-    featuresWithHighestAccuracy = []
-    accuracyFlag = 0
-    print("Using all features and \"random\" evaluation, I get an accuracy of ", highestAccuracy, "%.\n", sep = "")
-
-    print("Beginning Search.\n")
-
-    # populate with all available features
-    inputFeatures(featuresWithHighestAccuracy, numOfFeatures)
-
-    while(len(featuresWithHighestAccuracy) != 0):
-        # iterate through each feature to check accuracy without it
-        for i in range(0,len(featuresWithHighestAccuracy)):
-            accuracy = round(random.random() * 100,1) 
-            print("Removing feature", featuresWithHighestAccuracy[i])
-            print("Using feature(s) {", end ="")
-            # display remaining features
-            for j in range(0,len(featuresWithHighestAccuracy)):
-                if (i != j):
-                    print(featuresWithHighestAccuracy[j], sep = "", end="")
-                    if(i == len(featuresWithHighestAccuracy) - 1 and j == len(featuresWithHighestAccuracy) - 2):
-                        break
-                    if(j != len(featuresWithHighestAccuracy) - 1):
-                        print(",", sep = "", end="")
-            print("} accuracy is ", accuracy, "%.", sep = "")
-            # check if config has higher accuracy than previous best
-            if(accuracy > highestAccuracy):
-                feature = featuresWithHighestAccuracy[i]
-                accuracyFlag = 1
-                highestAccuracy = accuracy
-        # if no better accuracy print warning and stop search
-        if(accuracyFlag == 0):
-            printWarning(featuresWithHighestAccuracy, highestAccuracy)
-            break
-        else:
-            featuresWithHighestAccuracy.remove(feature)
-            printBest(featuresWithHighestAccuracy, highestAccuracy)
-            accuracyFlag = 0
 
 def calculateEuclideanDistance(features, test, train):
     distance = 0
-    if(isinstance(features,int)):
-        for i in range(0,features):
-            distance += (test[i+1] - train[i+1]) ** 2
-    else:
-        for i in range(0,len(features)):
-            distance += (test[features[i]] - train[features[i]]) ** 2
-    return distance ** 0.5
+    for feature in features:
+        distance += (test[feature] - train[feature]) ** 2
+    return np.sqrt(distance)
+
+
+def leaveOneOutValidation(features, df):
+    correct = 0
+    for i in range(len(df)):
+        test_instance = df.iloc[i]
+        train_data = df.drop(index=i)
+
+        nearest_neighbor = None
+        smallest_distance = float('inf')
+
+        for _, train_instance in train_data.iterrows():
+            distance = calculateEuclideanDistance(features, test_instance, train_instance)
+            if distance < smallest_distance:
+                smallest_distance = distance
+                nearest_neighbor = train_instance
+
+        if nearest_neighbor[0] == test_instance[0]:  # Compare class labels
+            correct += 1
+
+    return correct / len(df) * 100
+
 
 def getData(userInput):
-    df = pd.read_csv(userInput, sep="\s+", header = None)
-    df_normalized = df.copy()
-    startTime = time.time()
-    for column in df_normalized:
-        if (column != 0):
-            df_normalized[column] = (df[column] - df[column].mean()) / df[column].std()
-    endTime = time.time()
-    print("Time to normalize data:", round(endTime-startTime,2), "seconds")
-    return df_normalized
+    df = pd.read_csv(userInput, sep="\s+", header=None)
+    start_time = time.time()
+    for column in df.columns[1:]:  # Normalize all columns except the target
+        df[column] = (df[column] - df[column].mean()) / df[column].std()
+    end_time = time.time()
+    print("Time to normalize data:", round(end_time - start_time, 2), "seconds")
+    return df
 
-def NNClassifier(featuresToUse, userInput):
-    accuracy = 0
-    count = 0
-    nearestNeighbor = 0
-    lowestDistance = 0
-    df_normalized = getData(userInput)
-    if(isinstance(featuresToUse,str)):
-        print("Using all features in", userInput)
-    elif((isinstance(featuresToUse,list)) and len(featuresToUse) == 0):
-        print("Using no features in", userInput)
-    else:
-        print("Using features [", end="")
-        printFeatures(featuresToUse)
-        print("] in", userInput)
-    startTime = time.time()
-    for i in range(0,df_normalized.shape[0]):
-        if((isinstance(featuresToUse,list)) and len(featuresToUse) == 0):
+
+def forwardSelection(numOfFeatures, df):
+    featuresWithHighestAccuracy = []
+    highestAccuracy = 0
+    features = list(range(1, numOfFeatures + 1))
+
+    print(f"Using no features and \"leave-one-out\" evaluation, accuracy is {highestAccuracy:.2f}%.\n")
+    print("Beginning search.\n")
+
+    while features:
+        bestFeatureThisLevel = None
+        for feature in features:
+            currentFeatures = featuresWithHighestAccuracy + [feature]
+            accuracy = leaveOneOutValidation(currentFeatures, df)
+            print(f"Using feature(s) {currentFeatures}, accuracy is {accuracy:.2f}%")
+
+            if accuracy > highestAccuracy:
+                highestAccuracy = accuracy
+                bestFeatureThisLevel = feature
+
+        if bestFeatureThisLevel:
+            features.remove(bestFeatureThisLevel)
+            featuresWithHighestAccuracy.append(bestFeatureThisLevel)
+            printBest(featuresWithHighestAccuracy, highestAccuracy)
+        else:
+            printWarning(featuresWithHighestAccuracy, highestAccuracy)
             break
-        flag = 0
-        test = df_normalized.loc[i]
-        for j in range(0,df_normalized.shape[0]):
-            train = df_normalized.loc[j]
-            if (i != j):
-                if(isinstance(featuresToUse,str)):
-                    distance = calculateEuclideanDistance(df_normalized.shape[1]-1, test, train)
-                else:
-                    distance = calculateEuclideanDistance(featuresToUse, test, train)
-                if (flag == 0):
-                    nearestNeighbor = j
-                    lowestDistance = distance
-                    flag = 1
-                if(distance < lowestDistance):
-                    nearestNeighbor = j
-                    lowestDistance = distance
-        if (test[0] == df_normalized.loc[nearestNeighbor][0]):
-            count += 1
-    accuracy = (count/df_normalized.shape[0]) * 100
-    endTime = time.time()
-    print("Time to calculate accuracy:", round(endTime-startTime,2), "seconds")
-    print("Accuracy: ", accuracy, "%", sep="")
+
+
+def backwardSelection(numOfFeatures, df):
+    featuresWithHighestAccuracy = list(range(1, numOfFeatures + 1))
+    highestAccuracy = leaveOneOutValidation(featuresWithHighestAccuracy, df)
+
+    print(f"Using all features and \"leave-one-out\" evaluation, accuracy is {highestAccuracy:.2f}%.\n")
+    print("Beginning search.\n")
+
+    while len(featuresWithHighestAccuracy) > 1:
+        bestFeatureThisLevel = None
+        for feature in featuresWithHighestAccuracy:
+            currentFeatures = [f for f in featuresWithHighestAccuracy if f != feature]
+            accuracy = leaveOneOutValidation(currentFeatures, df)
+            print(f"Using feature(s) {currentFeatures}, accuracy is {accuracy:.2f}%")
+
+            if accuracy > highestAccuracy:
+                highestAccuracy = accuracy
+                bestFeatureThisLevel = feature
+
+        if bestFeatureThisLevel:
+            featuresWithHighestAccuracy.remove(bestFeatureThisLevel)
+            printBest(featuresWithHighestAccuracy, highestAccuracy)
+        else:
+            printWarning(featuresWithHighestAccuracy, highestAccuracy)
+            break
--- main.py
+++ main.py
@@ -1,28 +1,50 @@
 import func
-# User prompts and validate choices
-# Goes to the correct algorithm that the user chooses
 
-# while True:
-#     numFeatures = input("Please enter total number of features: ")
-#     try:
-#         numFeatures = int(numFeatures)
-#         break
-#     except ValueError:
-#         print("Please enter a integer")
+def specialAlgorithm(dataset_path):
+    df = func.getData(dataset_path)
+    num_features = df.shape[1] - 1  # Exclude the class label
+    print(f"Dataset loaded with {num_features} features.")
 
-# while True:
-#     numberAlgorithm = input("\nForward Selection(1)\nBacward Elimination(2)\nSpecial Algorithm(3)\nEnter:")
-#     try:
-#         numberAlgorithm = int(numberAlgorithm)
-#         break
-#     except ValueError:
-#         print("Please enter a integer")
+    print("\nRunning Forward Selection:")
+    func.forwardSelection(num_features, df)
 
-# if(numberAlgorithm == 1):
-#     func.forwardSelection(numFeatures)
+    print("\nRunning Backward Elimination:")
+    func.backwardSelection(num_features, df)
 
-# elif(numberAlgorithm == 2):
-#     func.backwardSelection(numFeatures)
 
-userInput = input("Type in the name of the file to test: ")
-func.NNClassifier([3,5,7], userInput)
+while True:
+    try:
+        userInput = input("Type in the name of the file to test: ")
+        dataset_path = userInput.strip()
+        with open(dataset_path, 'r') as f:
+            print(f"File '{dataset_path}' found and ready for processing.")
+        break
+    except FileNotFoundError:
+        print("File not found. Please try again.")
+
+while True:
+    numberAlgorithm = input(
+        "\nChoose an algorithm:\n"
+        "1. Forward Selection\n"
+        "2. Backward Elimination\n"
+        "3. Special Algorithm (All Tests)\n"
+        "Enter your choice: "
+    )
+    try:
+        numberAlgorithm = int(numberAlgorithm)
+        if numberAlgorithm in [1, 2, 3]:
+            break
+        else:
+            print("Please choose 1, 2, or 3.")
+    except ValueError:
+        print("Please enter a valid integer (1, 2, or 3).")
+
+df = func.getData(dataset_path)
+numFeatures = df.shape[1] - 1
+
+if numberAlgorithm == 1:
+    func.forwardSelection(numFeatures, df)
+elif numberAlgorithm == 2:
+    func.backwardSelection(numFeatures, df)
+elif numberAlgorithm == 3:
+    specialAlgorithm(dataset_path)
